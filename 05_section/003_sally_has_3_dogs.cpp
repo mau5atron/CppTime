2195c1b47575a8b185cffb903a4cfe98424e1709
Gabriel Betancourt <gabrielbetancourt96@outlook.com>
2019-05-13 10:58:41 -0400

    added sections

---
+++ 05_section/003_sally_has_3_dogs.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  int x = 3;
+  cout << "Sally has " << to_string(x) << " dogs" << endl;
+  return 0;
+}
---
+++ 06_section/004_constants.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  // literal constants
+  int x {12};
+  double y {1.56};
+  string name {"Frank"};
+  string middle_initial {"J"};
+
+  // integer literal constants 
+  int z {12};
+  int q {12U};
+
+  return 0;
+}
