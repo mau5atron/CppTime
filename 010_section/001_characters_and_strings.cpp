f945b87af425abfba37f3ef18108b9dbedb107e6
Gabriel Betancourt <gabrielbetancourt96@outlook.com>
2019-11-01 00:02:41 -0400

    ➕ added C-style string notes

---
+++ 010_section/001_characters_and_strings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(void)
+{ 
+  return 0;
+}
+
+/*
+  Section Overview
+
+  Characters and Strings
+
+  - Character functions
+  - C-style Strings 
+  - Working with C-style strings
+  - C++ Strings
+  - Working with C++ Strings
+
+  
+*/ 
---
+++ 010_section/002_character_functions.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main(void)
+{
+  return 0;
+}
+
+/*
+  Character Functions
+
+  #include <cctype>
+
+  function_name(char)
+
+  - Functions for testing characters
+  - Functions for converting character case
+
+  - <cctype> inclues simple and useful functions that allow the testing of
+    characters for various properties as well as the conversion of characters
+    from upper to lower or lower to upper case in order to use these functions.
+  - <cctype> functions expect a single character.
+
+  Testing characters: 
+
+  isalpha(c) - True if c is a letter
+  isalnum(c) - True if c is a letter or a digit 
+  isdigit(c) - True if c is a digit
+  islower(c) - True if c is lowercase letter
+  isprint(c) - True if c is a printable character 
+  ispunct(c) - True if c is a punctuation character
+  isupper(c) - True if c is an uppercase letter
+  isspace(c) - True if c is whitespace 
+
+  Converting characters: 
+
+  tolower(c) - Returns lowercase of c
+  toupper(c) - Returns uppercase of c
+*/ 
