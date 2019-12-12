1ba0202b0610c93ee8ef65e925a317a23633d448
mau5atron <gabrielbetancourt96@outlook.com>
2019-12-12 18:05:27 -0500

    Random nums

---
+++ 011_section/003_random.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+typedef unsigned int uint;
+
+int main(void)
+{
+  int random_num {};
+  size_t count { 10 }; // count of rand nums to generate 
+  uint min { 1 }; // lower bound
+  uint max { 6 }; // high bound
+
+  // seed the random num generator 
+  // if you don't seed the generator you will get the same sequence of random
+  // numbers on every run
+ 
+  return 0;
+}
+
+/*
+
+*/ 
