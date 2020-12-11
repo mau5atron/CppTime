efb594e26ba1746bd29fd01330d712d7c657fb6d
mau5atron <gabrielbetancourt96@outlook.com>
2020-12-11 00:27:08 -0500

    added to section 19, module 18

--- 019_io_and_streams/018_using_string_streams.cpp
+++ 019_io_and_streams/018_using_string_streams.cpp
@@ -23,5 +23,68 @@
       #include <sstream>
 
       int num {};
+      double total {};
+      std::string name {};
+      std::string info {"Moe 100 1234.5"};
+
+      std::istringstream iss { info }; // input string stream
+      iss >> name >> num >> total;
     }
 */
+
+/*
+  Writing to a stringstream
+
+    {
+      #include <sstream>
+
+      int num { 100 };
+      double total { 1234.5 };
+      std::string name { "Moe" };
+
+      std::ostringstream oss {}; // output string stream
+      oss << name << " " << num << " " << total;
+      std::cout << oss.str() << "\n";
+    }
+*/
+
+/*
+  Validating Input with stringstream
+
+    {
+      int value {};
+      std::string input {};
+
+      std::cout << "Enter an integer: ";
+      std::cin >> input;
+
+      std::stringstream ss { input };
+      if ( ss >> value ){
+        std::cout << "An integer was entered";
+      } else {
+        std::cout << "An integer was NOT entered";
+      }
+    }
+*/
+
+// stringstream example
+
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <iostream>
+
+int main(void){
+  int num {};
+  double total {};
+  std::string name {};
+
+  std::string info { "Moe 100 1234.5" };
+  std::istringstream input_string_stream { info };
+
+  std::cout
+    << std::setw(10) << std::left << name
+    << std::setw(5) << num
+
+  return 0;
+}
